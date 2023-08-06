f5feea5b00c701573c20b56266b6b2fe1aebc534
david <davidjuradoyt@gmail.com>
2023-08-06 18:14:28 -0500

    prueba

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  integrations: [react(), tailwind()],  
   site: 'https://yesidexe.github.io',
-  integrations: [react(), tailwind()]
 });
