96739c8783b61bfa074864b34bcac96f6b5a1e32
david <davidjuradoyt@gmail.com>
2023-08-06 20:14:03 -0500

    netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
+import netlify from '@astrojs/netlify/functions';
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
+  adapter: netlify(),
   integrations: [react(), tailwind()],  
 });
